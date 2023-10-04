36275eb93c0a6dd7cf62f0a8e93e5f8478229b53
Christian Salgård Svinding <142394086+cssv56187@users.noreply.github.com>
2023-10-04 11:32:32 +0200

    shit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcApiProject.Models;
+using SharedModels;
 using SharedModels.Models;
 using System.Diagnostics;
 
@@ -22,15 +23,15 @@ namespace MvcApiProject.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostCity(string city, string countryCode)
+        public async Task<IActionResult> PostCity(string city, string countryCode, int days)
         {
-            var url = $"api/Country?city={city}&countryCode={countryCode}";
+            var url = $"api/v1/Country?city={city}&countryCode={countryCode}";
             try
             {
                 var countries = await client.GetFromJsonAsync<List<CountrySelector>>(url);
                 if (countries != null)
                 {
-                    return RedirectToAction("City", new { lat = countries[0].lat, lon = countries[0].lon, countryCode = countryCode, city = city });
+                    return RedirectToAction("City", new { lat = countries[0].lat, lon = countries[0].lon, countryCode = countryCode, city = city, days = days });
                 }
             }
             catch (Exception ex)
@@ -40,14 +41,42 @@ namespace MvcApiProject.Controllers
             return View(null);
         }
 
-        public async Task<IActionResult> City(double lat, double lon, string countryCode, string city)
+        public async Task<IActionResult> City(double lat, double lon, string countryCode, string city, string days)
         {
-            var url = $"api/Weather?lat={lat}&lon={lon}";
+            var url = $"api/v1/Weather?lat={lat}&lon={lon}";
             try
             {
                 ViewData["city"] = city;
                 ViewData["countryCode"] = countryCode;
-                var weatherData = await client.GetFromJsonAsync<WeatherData>(url);
+                var weatherData = await client.GetFromJsonAsync<OpenWeatherData>(url);
+                if (weatherData != null)
+                {
+                    List<DateTime> dates = new List<DateTime>();
+                    for(int i = 0; i < weatherData.list.Count; i++)
+                    {
+                        DateTime date = DateTimeOffset.FromUnixTimeSeconds(weatherData.list[i].dt).DateTime;
+                        if (!dates.Contains(date.Date))
+                        {
+                            dates.Add(date.Date);
+                        }
+                    }
+                    return View(weatherData);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return View(null);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Stormglass()
+        {
+            var url = $"api/v2/Stormglass";
+            try
+            {
+                var weatherData = await client.GetFromJsonAsync<StormglassData>(url);
                 if (weatherData != null)
                 {
                     return View(weatherData);
